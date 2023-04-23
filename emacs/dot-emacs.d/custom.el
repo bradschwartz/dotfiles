820ee95a774fdd3a1f18b096b835d292217f5968
bradschwartz <baschwartz95@gmail.com>
2023-04-22 22:35:15 -0700

    rando custom emacs stuff

--- emacs/dot-emacs.d/custom.el
+++ emacs/dot-emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(docker-tramp devcontainer paredit lsp-mode go-mode company use-package)))
+   '(terraform-mode docker-tramp devcontainer paredit lsp-mode go-mode company use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
