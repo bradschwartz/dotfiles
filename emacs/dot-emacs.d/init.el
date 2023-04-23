2e6df7ee7b81c2ad7fd1f0e37848aa89bd239f3b
bradschwartz <baschwartz95@gmail.com>
2023-04-22 23:26:00 -0700

    Move all package configs after gnu default settings, just in case

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -5,6 +5,24 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(when (file-exists-p "~/.emacs.d/debug-hooks.el") (load "~/.emacs.d/debug-hooks.el"))
+
+(setq column-number-mode t)
+(setq show-paren-mode t)
+(setq make-backup-files nil) ; stop creating backup~ files
+;; auto backup to ~/.emacs.d/backups
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
+(setq auto-save-default nil) ; stop creating #autosave# files
+(setq vc-follow-symlinks t)
+;; issue on Mac when opening entire dirs: https://stackoverflow.com/a/56096775
+(when (string= system-type "darwin")
+  (setq dired-use-ls-dired nil))
+
+;; Don't clutter init files with auto-inserted settings
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; using `use-package` to ensure all packages I want are installed
 ;; Globally allow `use-package` to always install if not available
 ;; This _must_ come first before any other package references/configurations
@@ -43,26 +61,8 @@
   :hook ((rust-mode . lsp-deferred))
   )
 
-(setq column-number-mode t)
-(setq show-paren-mode t)
-(setq make-backup-files nil) ; stop creating backup~ files
-;; auto backup to ~/.emacs.d/backups
-(setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
-(setq auto-save-default nil) ; stop creating #autosave# files
-(setq vc-follow-symlinks t)
-;; issue on Mac when opening entire dirs: https://stackoverflow.com/a/56096775
-(when (string= system-type "darwin")
-  (setq dired-use-ls-dired nil))
-
-;; Don't clutter init files with auto-inserted settings
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
 ;; Section for any local working stuff
 (use-package devcontainer
   :load-path "~/code/bradschwartz/devcontainer.el/"
   :hook ((dired-before-readin . devcontainer-dir-open-hook))
   )
-
-(when (file-exists-p "~/.emacs.d/debug-hooks.el") (load "~/.emacs.d/debug-hooks.el"))
