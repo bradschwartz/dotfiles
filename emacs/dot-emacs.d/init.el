869e3e1c302c64a1a4cbe6357c04bc8d786f42c5
bradschwartz <baschwartz95@gmail.com>
2023-05-01 14:11:19 -0700

    Switching to built-in treesitter package for major language modes

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -76,18 +76,34 @@
   (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
   )
 
-;; language modes
-(use-package go-mode
+
+
+;; tree-sitter is built-in in emacs@29
+;; using Rob's package for autosetting grammar repos, and handling major-mode-remap-alist
+;; src: https://robbmann.io/posts/emacs-treesit-auto/
+(require 'treesit)
+(use-package treesit-auto
+  :demand t ;; forces use-package to always load on startup, regardless of mode bindings
+  :config
+  (setq treesit-auto-install 'prompt)
+  (global-treesit-auto-mode)
+  )
+
+;; Make sure to update this with any new language servers installed!
+;; This removes the need to have (use-package ${lang}-mode :hook ((${lang}-mode . eglot-ensure)))
+;; since we use tree-sitter for syntax now, at least when supported
+(use-package eglot
   :hook (
-	 (go-mode . eglot-ensure)
-	 (before-save . eglot-format-buffer)
-	 (before-save . eglot-code-action-organize-imports)
+	 (go-ts-mode . eglot-ensure)
+	 (rust-ts-mode . eglot-ensure)
+	 (terraform-mode . eglot-ensure)
 	 )
   )
+
 ;; sets up inlay hints
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
-	       '((go-mode) .
+	       '((go-ts-mode) .
 		 ("gopls" :initializationOptions
 		  (:hints (:parameterNames t
 					   :rangeVariableTypes t
@@ -97,9 +113,6 @@
 					   :compositeLiteralTypes t
 					   :constantValues t)))))
   )
-(use-package rust-mode
-  :hook ((rust-mode . eglot-ensure))
-  )
 
 ;; validate elisp packages, required for melpa upload
 ;; https://github.com/purcell/package-lint
@@ -107,10 +120,8 @@
 
 ;; terraform language settings
 ;; lsp was installed with `brew install hashicorp/tap/terraform-ls`
-(use-package terraform-mode
-  :hook (
-	 (terraform-mode . eglot-ensure))
-  )
+(use-package terraform-mode)
+
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
 	       '(terraform-mode . ("terraform-ls" "serve"))
