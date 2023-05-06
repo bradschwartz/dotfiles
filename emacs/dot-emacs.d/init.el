4e54c6eeda961d6d6333dad21745499a907e2c2e
bradschwartz <baschwartz95@gmail.com>
2023-05-06 15:41:22 -0700

    Adding editorconfig to make github browsing eaiser

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -4,14 +4,14 @@
 
 ;; Lower threshold back to 8 MiB (default is 800kB)
 (add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold (expt 2 23))))
+	(lambda ()
+		(setq gc-cons-threshold (expt 2 23))))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+	;; For important compatibility libraries like cl-lib
+	(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (when (file-exists-p "~/.emacs.d/debug-hooks.el") (load "~/.emacs.d/debug-hooks.el"))
@@ -22,12 +22,12 @@
 (setq make-backup-files nil) ; stop creating backup~ files
 ;; auto backup to ~/.emacs.d/backups
 (setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
+	`(("." . ,(concat user-emacs-directory "backups"))))
 (setq auto-save-default nil) ; stop creating #autosave# files
 (setq vc-follow-symlinks t)
 ;; issue on Mac when opening entire dirs: https://stackoverflow.com/a/56096775
 (when (string= system-type "darwin")
-  (setq dired-use-ls-dired nil))
+	(setq dired-use-ls-dired nil))
 
 ;; Don't clutter init files with auto-inserted settings
 (setq custom-file "~/.emacs.d/custom.el")
@@ -51,31 +51,31 @@
 
 (use-package magit)
 (use-package editorconfig
-  :config
-  (editorconfig-mode 1)
-  )
+	:config
+	(editorconfig-mode 1)
+	)
 (use-package try)
 
 (use-package markdown-mode)
 (use-package yaml-mode)
 
 (use-package company
-  :hook (
-	 (prog-mode))
-  )
+	:hook (
+					(prog-mode))
+	)
 (use-package paredit
-  ;; Active paredit automatically: https://www.emacswiki.org/emacs/ParEdit#h5o-1
-  :hook (
-	 (emacs-lisp-mode . enable-paredit-mode)
-	 ;; disabled for: https://www.reddit.com/r/emacs/comments/101uwgd/enable_paredit_mode_for_evalexpression_mini/
-	 ;;(eval-expression-minibuffer-setup . enable-paredit-mode)
-	 (ielm-mode . enable-paredit-mode)
-	 (lisp-mode . enable-paredit-mode)
-	 (lisp-interaction-mode . enable-paredit-mode)
-	 (scheme-mode . enable-paredit-mode))
-  :init
-  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-  )
+	;; Active paredit automatically: https://www.emacswiki.org/emacs/ParEdit#h5o-1
+	:hook (
+					(emacs-lisp-mode . enable-paredit-mode)
+					;; disabled for: https://www.reddit.com/r/emacs/comments/101uwgd/enable_paredit_mode_for_evalexpression_mini/
+					;;(eval-expression-minibuffer-setup . enable-paredit-mode)
+					(ielm-mode . enable-paredit-mode)
+					(lisp-mode . enable-paredit-mode)
+					(lisp-interaction-mode . enable-paredit-mode)
+					(scheme-mode . enable-paredit-mode))
+	:init
+	(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+	)
 
 (use-package terraform-mode) ;; lsp was installed with `brew install hashicorp/tap/terraform-ls`
 
@@ -83,11 +83,11 @@
 ;; using Rob's package for autosetting grammar repos, and handling major-mode-remap-alist
 ;; src: https://robbmann.io/posts/emacs-treesit-auto/
 (use-package treesit-auto
-  :demand t ;; forces use-package to always load on startup, regardless of mode bindings
-  :config
-  (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode)
-  )
+	:demand t ;; forces use-package to always load on startup, regardless of mode bindings
+	:config
+	(setq treesit-auto-install 'prompt)
+	(global-treesit-auto-mode)
+	)
 
 ;; emacs@29 has built-in treesitter modes, but doesn't auto enable them
 ;; could add ${lang}-mode packages, but why bloat? just add files ourselves to alist to auto-enable
@@ -98,24 +98,24 @@
 ;; This removes the need to have (use-package ${lang}-mode :hook ((${lang}-mode . eglot-ensure)))
 ;; since we use tree-sitter for syntax now, at least when supported
 (use-package eglot
-  :hook (
-				 (go-ts-mode . eglot-ensure)
-				 (rust-ts-mode . eglot-ensure)
-				 (terraform-mode . eglot-ensure)
-				 )
-  :config
-  (add-to-list 'eglot-server-programs '(terraform-mode . ("terraform-ls" "serve")))
+	:hook (
+					(go-ts-mode . eglot-ensure)
+					(rust-ts-mode . eglot-ensure)
+					(terraform-mode . eglot-ensure)
+					)
+	:config
+	(add-to-list 'eglot-server-programs '(terraform-mode . ("terraform-ls" "serve")))
 	(add-to-list 'eglot-server-programs
-							 '((go-ts-mode go-mode) .
-								 ("gopls" :initializationOptions
-									(:hints (:parameterNames t
-																					 :rangeVariableTypes t
-																					 :functionTypeParameters t
-																					 :assignVariableTypes t
-																					 :compositeLiteralFields t
-																					 :compositeLiteralTypes t
-																					 :constantValues t)))))
-  )
+		'((go-ts-mode go-mode) .
+			 ("gopls" :initializationOptions
+				 (:hints (:parameterNames t
+									 :rangeVariableTypes t
+									 :functionTypeParameters t
+									 :assignVariableTypes t
+									 :compositeLiteralFields t
+									 :compositeLiteralTypes t
+									 :constantValues t)))))
+	)
 
 ;; validate elisp packages, required for melpa upload
 ;; https://github.com/purcell/package-lint
@@ -123,6 +123,6 @@
 
 ;; Section for any local working stuff
 (use-package devcontainer
-  :load-path "~/code/bradschwartz/devcontainer.el/"
-  :hook ((dired-before-readin . devcontainer-dir-open-hook))
-  )
+	:load-path "~/code/bradschwartz/devcontainer.el/"
+	:hook ((dired-before-readin . devcontainer-dir-open-hook))
+	)
