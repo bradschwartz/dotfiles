28a25bd07b29c88796aedf7dc19979d7233c0fcc
bradschwartz <baschwartz95@gmail.com>
2024-02-29 15:21:04 -0800

    make wezterm light/dark mode aware. Still don't love it

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -12,8 +12,24 @@ end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+-- For example, changing the color scheme
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Light'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Frappe'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 -- and finally, return the configuration to wezterm
 return config
