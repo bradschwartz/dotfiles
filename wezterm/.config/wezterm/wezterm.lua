04efd4226827607618054b3af3b48ee4d10dfec3
bradschwartz <baschwartz95@gmail.com>
2024-08-12 18:19:13 -0700

    Update wezterm for my keybinding preferences, set tab title based on pwd

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -31,5 +31,55 @@ function scheme_for_appearance(appearance)
 end
 config.color_scheme = scheme_for_appearance(get_appearance())
 
+-- Remove all path components and return only the last value
+local function remove_abs_path(path) return path:gsub("(.*[/\\])(.*)", "%2") end
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- current working directory
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then return title end
+	local pwd_path = tab_info.active_pane.current_working_dir.file_path
+	return remove_abs_path(pwd_path)
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local title = tab_title(tab)
+    if tab.is_active then
+      return {
+        { Background = { Color = 'blue' } },
+        { Text = ' ' .. title .. ' ' },
+      }
+    end
+    return title
+  end
+)
+
+
+
+config.keys = {
+	-- disable ctrl+ - for emacs to use for undo
+	{
+		mods = 'CTRL',
+		key = '-',
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		mods = 'CTRL',
+		key = '=',
+		action = wezterm.action.Nop,
+	},
+	{
+		mods = 'CMD',
+		key = 'k',
+		action = wezterm.action.ClearScrollback 'ScrollbackAndViewport'
+	}
+}
+
 -- and finally, return the configuration to wezterm
 return config
